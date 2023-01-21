c820afa741ab12b1d3ccb1670781cc5605b0a127
Juan Tumini <juantumini5@gmail.com>
2023-01-21 16:39:19 -0300

    validacion de form

--- script.js/script.js
+++ script.js/script.js
@@ -4,14 +4,14 @@ const btn = document.getElementById("btn")
 
 
 class goles {
-        constructor(local, visitante, apuesta,ganador,ganancia){
-            this.local = local;
-            this.visitante = visitante;
-            this.apuesta = apuesta;
-            this.ganador = ganador;
-            this.ganancia = ganancia;                   
-        }
+    constructor(local, visitante, apuesta,ganador,ganancia){
+        this.local = local;
+        this.visitante = visitante;
+        this.apuesta = apuesta;
+        this.ganador = ganador;
+        this.ganancia = ganancia;                   
     }
+}
 
 const Argentina = 1.25
 const Francia = 1.72
@@ -29,7 +29,6 @@ const nuevaApuesta = () => {
     let local = document.getElementById("local").value;
     let visitante = document.getElementById("visitante").value;
     let apuesta = document.getElementById("apuesta").value;
-    
     if(local > visitante){
         ganador = "Argentina"
         ganancia = parseInt(calcularApuesta(apuesta,Argentina)) 
@@ -49,26 +48,18 @@ const nuevaApuesta = () => {
 
 
 const promedioApuestas = () => { 
-    let sumaEl = listaApuestas.reduce((total, listaApuestas) => {
-        return total + parseInt(listaApuestas.local) +  parseInt(listaApuestas.visitante)
-    }, 0)
-    let cantidadEl = listaApuestas.length  
-    let promedio = Math.round(sumaEl / cantidadEl);
-    divApuestas.innerHTML += `<div class='apuestas'><h3>El promedio de goles por apuesta es de:${promedio}</h3></div>`
+    if(listaApuestas.length > 0){
+        let sumaEl = listaApuestas.reduce((total, listaApuestas) => {
+            return total + parseInt(listaApuestas.local) +  parseInt(listaApuestas.visitante)
+        }, 0)
+        let cantidadEl = listaApuestas.length  
+        let promedio = Math.round(sumaEl / cantidadEl);
+        divApuestas.innerHTML += `<div class='apuestas'><h3>El promedio de goles por apuesta es de:${promedio}</h3></div>`
+    }
 }
 
-// const mostrarApuestas = () => { 
-//     listaApuestas.forEach (goles => {
-//         divApuestas.innerHTML += `
-//             <div class='apuestas'>
-//             <h3>Tu apuesta por $${goles.apuesta} a Argentina:${goles.local} - Francia:${goles.visitante} 
-//             . tiene una Ganancia de: $${goles.ganancia}</h3>
-//             </div>
-//             `
-//     })
-// }
 const mostrarApuestas = () => {
-    divApuestas.innerHTML = ""
+    divApuestas.innerHTML = "<h2>Tus Apuestas:</h2>"
     listaApuestas.forEach (goles => {
         divApuestas.innerHTML += `
             <div class='apuestas'>
@@ -78,6 +69,9 @@ const mostrarApuestas = () => {
         `
     })
 }
+mostrarApuestas();
+promedioApuestas();
+
 //////////////////// VALIDACION Y SUBMIT DEL FORMULARIO ////////////////////
 const form = document.getElementById('form');
 
@@ -109,13 +103,9 @@ const validateForm = (local, visitante, apuesta) => {
     return new Promise((resolve, reject) => {
         if (!local || !visitante || !apuesta) {
             reject('Todos los campos son necesarios');
-        } else if (isNaN(local) || isNaN(visitante) || isNaN(apuesta)) {
-            reject('Los campos deben contener solo numeros');
         } else {
             resolve();
         }
     });
 }
 //////////////////////////////////////////////////////////////
-// promedioApuestas();
-// mostrarApuestas();
